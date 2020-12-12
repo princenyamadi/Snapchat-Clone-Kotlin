9c33d92ebd068ef389178ed9b174bb314bab160e
princenyamadi <princenyamadi.pn@gmail.com>
2020-12-12 22:05:00 +0000

    [feat]: Added UI and initialized widgets

--- app/src/main/java/tech/peny/snapchatclone/MainActivity.kt
+++ app/src/main/java/tech/peny/snapchatclone/MainActivity.kt
@@ -2,10 +2,28 @@ package tech.peny.snapchatclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    var emailEditText: EditText? = null
+    var passwordEditText: EditText? = null
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+    }
+
+
+    fun goClicked(view: View){
+
     }
 }
